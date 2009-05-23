ea0ab48b1b3ee9c3ac0ed829f98b1c1946050225
grepz <grepz@jarios.org>
2009-05-23 14:48:23 +0400

    * emacs-rc/network/rc-newsticker.el: Changes to newsticker.

--- emacs-rc/network/rc-newsticker.el
+++ emacs-rc/network/rc-newsticker.el
@@ -1,6 +1,6 @@
 ;; Elisp source code header -*- coding: utf-8 -*-
 ;; Created: [10.21:39 Май 22 2009]
-;; Modified: [01.05:28 Май 23 2009]
+;; Modified: [14.46:52 Май 23 2009]
 ;; Description: 
 ;; Author: Stanislav M. Ivankin
 ;; Email: stas@concat.info
@@ -8,19 +8,33 @@
 ;; License: 
 
 
-;;(load-file "/usr/share/emacs/23.0.92/lisp/gnus/mailcap.el.gz")
+;;(load-file "/usr/share/emacs/23.0.93/lisp/gnus/mailcap.el.gz")
+
+(custom-set-variables
+ '(newsticker-retrieval-interval -1))
+ 
 
 (autoload 'newsticker-start "newsticker" "Emacs Newsticker" t)
 (autoload 'newsticker-show-news "newsticker" "Emacs Newsticker" t)
 
 (custom-set-variables
  '(newsticker-url-list
-   '(("EmacsWiki Recently Change"
-      "http://www.emacswiki.org/emacs/index.rss" nil nil nil)
-     ("Planet Emacsen"
+   '(("Planet Emacsen"
       "http://emacs.defun.ru/atom.xml" nil nil nil)
      ("Kernel Trap"
-      "http://kerneltrap.org/node/feed" nil nil nil))))
+      "http://kerneltrap.org/node/feed" nil nil nil)
+     ("Reddit - Technology"
+      "http://www.reddit.com/r/technology/.rss" nil nil nil)
+     ("Reddit - Science"
+      "http://www.reddit.com/r/science/.rss" nil nil nil)
+     ("Reddit - Worldnews"
+      "http://www.reddit.com/r/worldnews/.rss" nil nil nil)
+     ("Reddit - Math"
+      "http://www.reddit.com/r/math/.rss" nil nil nil)
+     ("Reddit - Programming"
+      "http://www.reddit.com/r/programming/.rss" nil nil nil)
+     ("Linux.org.ru"
+      "http://www.linux.org.ru/section-rss.jsp?section=1" nil nil nil))))
 
 (provide 'rc-newsticker)
 ;;; rc-newsticker.el ends here
